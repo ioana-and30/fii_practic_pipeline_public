b7778664d27ad226596459fc5a3b2ef30a4378aa
ioana-and30 <157801961+ioana-and30@users.noreply.github.com>
2025-04-12 12:09:31 +0300

    Clean-up

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
-variable "azs" {
-  type        = list(string)
-  description = "Availability zones for the region"
-  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-}
+# variable "azs" {
+#   type        = list(string)
+#   description = "Availability zones for the region"
+#   default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+# }
 
-variable "org" {
-  type        = string
-  description = "The organization, short name"
-  default     = "FIIpractic"
-}
+# variable "org" {
+#   type        = string
+#   description = "The organization, short name"
+#   default     = "FIIpractic"
+# }
 
-variable "ami" {
-  type        = string
-  description = "The AMI ID for the instance"
-  default     = "ami-01ff9fc7721895c6b"
-}
+# variable "ami" {
+#   type        = string
+#   description = "The AMI ID for the instance"
+#   default     = "ami-01ff9fc7721895c6b"
+# }
 
-variable "vpc_cidr" {
-  type        = string
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
-}
+# variable "vpc_cidr" {
+#   type        = string
+#   description = "CIDR block for the VPC"
+#   default     = "10.0.0.0/16"
+# }
